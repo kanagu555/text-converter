beca950e8541cd1f13e00b3fb8efe0a75a436fa8
Kanagaraj K <Kanagarajwhb@gmail.com>
2023-07-11 22:24:08 +0530

    copy btn success msg update

--- src/components/ButtonFields/CopyBtn.js
+++ src/components/ButtonFields/CopyBtn.js
@@ -1,27 +1,33 @@
-import { Button } from '@mui/material'
-import React, { useContext } from 'react'
-import {AppContext} from './../../App'
-import {MainFormContext} from '../../../src/components/FormFields/MainForm'
+import { Button } from "@mui/material";
+import React, { useContext } from "react";
+import { AppContext } from "./../../App";
 
 function CopyBtn() {
-
-  const copyBtnContext = useContext(AppContext)
-  const copySuccessMsg = useContext(MainFormContext)
-  const outputValue = copyBtnContext[2]
-  const copyMsg = copySuccessMsg[0];
-
-  console.log('copySuccessMsg:', copySuccessMsg[0]);
+  const copyBtnContext = useContext(AppContext);
+  const [outputValue, setCopySuccess] = [copyBtnContext[2], copyBtnContext[4]];
 
   const copyFunc = () => {
-    navigator.clipboard.writeText(outputValue)
-    copyMsg(true)
-  }
+    if (outputValue) {
+      navigator.clipboard.writeText(outputValue);
+      setCopySuccess((prevState) => ({
+        ...prevState,
+        lowerCaseSuccess: false,
+        upperCaseSuccess: false,
+        camelCaseSuccess: false,
+        mixedCaseSuccess: false,
+        inverseCaseSuccess: false,
+        copySuccess: true,
+      }));
+    }
+  };
 
   return (
     <div className="btn-margin">
-        <Button variant="contained" color="success" onClick={copyFunc}>Copy</Button>
+      <Button variant="contained" color="success" onClick={copyFunc}>
+        Copy
+      </Button>
     </div>
-  )
+  );
 }
 
-export default CopyBtn
+export default CopyBtn;
